07c7bec96a51f1b6077523287b4973e13204e71d
moabdelaziem123 <144205494+moabdelaziem123@users.noreply.github.com>
2023-09-24 17:42:42 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+let mode = document.querySelector('#mode');
+
+darkmode.onclick = () =>{
+if(darkmode.classList.contains('bx-moon')){
+darkmode.classList.replace('bx-moon' , 'bx-sun');
+document.body.classList.add('color');
+
+}else{
+darkmode.classList.replace('bx-sun' , 'bx-moon');
+document.body.classList.remove('color');
+}
+}
